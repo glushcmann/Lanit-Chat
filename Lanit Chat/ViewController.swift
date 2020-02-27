b88c91ec54de1b559d4c87be83c8b861db4680f9
Nick <nikita.glu@icloud.com>
2020-02-27 15:24:18 +0300

    Initial Commit

---
+++ Lanit Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lanit Chat
+//
+//  Created by Никита on 27.02.2020.
+//  Copyright © 2020 Nikita Glushchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
