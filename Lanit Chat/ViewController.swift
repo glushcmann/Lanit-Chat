716516c0bbd5444334e5653f5a1ea4008def88f5
Nick <nikita.glu@icloud.com>
2020-03-01 17:16:50 +0300

    first commit

--- Lanit Chat/ViewController.swift
+++ Lanit Chat/ViewController.swift
@@ -13,8 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("Controller method: \(#function)")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("Controller method: \(#function)")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("Controller method: \(#function)")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        print("Controller method: \(#function)")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("Controller method: \(#function)")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("Controller method: \(#function)")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("Controller method: \(#function)")
+    }
 }
 
